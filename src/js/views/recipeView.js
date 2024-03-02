3edba4d72e64bf01be5f16e33214649ed9aff503
Devansh27112001 <dkansara2711@gmail.com>
2024-03-02 15:30:55 -0500

    Changed a dependency

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from '../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './View';
 
 // console.log(Fraction);
@@ -135,7 +135,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+        ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ingredient.unit}</span>
